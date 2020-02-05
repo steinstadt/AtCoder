704e691b4419054f44e52a0bfbb50bec735ac730
keisuke <keisuke@db.ics.keio.ac.jp>
2020-02-05 15:54:45 +0900

    tutorial removed

--- tutorial/test3.py
+++
@@ -1,6 +0,0 @@
-N, M = map(int, input().split())
-
-if M==N:
-    print("Yes")
-else:
-    print("No")
--- tutorial/test4.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-a_str = str(a)*b
-b_str = str(b)*a
-
-if a_str <= b_str:
-    print(a_str)
-else:
-    print(b_str)
